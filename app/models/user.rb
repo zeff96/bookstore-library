9b140a5e6912401eacf24f178e7cba82603e98b6
zeff96 <zeffo1975@gmail.com>
2025-02-12 00:39:05 +0300

    Add validations for email address and passwords for user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  validates :email_address, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+end
