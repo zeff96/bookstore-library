5b8bdb67639622409644633ba0689ba15ea19aa0
zeff96 <zeffo1975@gmail.com>
2025-02-12 20:06:58 +0300

    Add routes for borrowing a book and returning a borrowed book

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resource :registration, only: %i[new create]
+  resources :users, only: %i[show]
+  resources :books, only: %i[index show] do
+    member do
+      post "borrow"
+      post "return_borrowed_book"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +20,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "books#index"
 end
