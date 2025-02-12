ed7484f9f57c14de71fb9634653bd7e27dbf1396
zeff96 <zeffo1975@gmail.com>
2025-02-12 19:59:58 +0300

    Add book model class with validations

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+  has_many :borrowings
+  has_many :users, through: :borrowings
+
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :isbn, presence: true, uniqueness: true
+
+  enum :status, [ :available, :borrowed ]
+end
