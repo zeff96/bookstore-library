98fbcad857a5cb957657fcddf4da0262fbb40d36
zeff96 <zeffo1975@gmail.com>
2025-02-12 20:00:26 +0300

    Add associtaion between user book and borrowing records

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :borrowings
+  has_many :books, through: :borrowings
+
+  has_one_attached :avatar
 
   validates :email_address, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
