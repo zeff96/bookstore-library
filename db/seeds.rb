58b1c2afae2b8509443c28b62b2985b9ac6aff65
zeff96 <zeffo1975@gmail.com>
2025-02-12 20:07:59 +0300

    Add seed for user creation and book creation list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+
+user = User.create!(
+  username: "Allan Smith",
+  email_address: "allan@example.com",
+  bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis perferendis doloribus ab aperiam eos placeat consequatur quia culpa repudiandae. Sit ut autem deleniti obcaecati ullam facilis iste nihil, totam nobis fugiat officiis ratione molestiae? Maiores unde vero iure explicabo iusto sint! Dolores obcaecati et perspiciatis optio, libero id doloribus ut!",
+  password: "password",
+  password_confirmation: "password"
+)
+
+
+avatar_path = Rails.root.join("public", "avatar", "pngwing.com.png")
+
+if File.exist?(avatar_path)
+  user.avatar.attach(
+    io: File.open(avatar_path),
+    filename: "pngwing.com.png",
+    content_type: "image/png"
+  )
+else
+  puts "Avatar file not found at #{avatar_path}"
+end
+
+12.times do |_|
+  Book.create!(
+    author: Faker::Book.author,
+    title: Faker::Book.title,
+    isbn: Faker::Code.isbn
+  )
+end
