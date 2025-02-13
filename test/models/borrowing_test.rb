0c1ce0b3a24c3a9531626deefe684badf2e4c373
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:56:11 +0300

    Add model test for borrowing model covering all associations and validations

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class BorrowingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # validations
+  setup do
+    @borrowing = create(:borrowing)
+  end
+
+  test "should not save without user_id" do
+    @borrowing.user = nil
+    assert_not @borrowing.save, "Saved borrowing without user id"
+  end
+
+  test "should not save without book id" do
+    @borrowing.book = nil
+    assert_not @borrowing.save, "Saved without book id"
+  end
+
+  test "should not save without borrowed at" do
+    @borrowing.borrowed_at = nil
+    assert_not @borrowing.save, "Saved without borrowed at"
+  end
+
+  test "should not save without due date" do
+    @borrowing.due_date = nil
+    assert_not @borrowing.save, "Saved without due date"
+  end
+
+  test "should save valid borrowing" do
+    assert @borrowing.save
+  end
+
+  # associations
+
+  test "should belong to user and book" do
+    book = create(:book)
+    user = create(:user)
+    borrowing = create(:borrowing, user: user, book: book)
+
+    assert_equal borrowing.user,  user
+    assert_equal borrowing.book, book
+  end
 end
