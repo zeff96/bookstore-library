bded8dd9e8b2fe67f4fb9b0cea74734381d12119
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:53:22 +0300

    Add controller test for books controller covering all actions in the books controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,8 +1,41 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @book = create(:book)
+    @user = create(:user)
+    sign_in @user
+  end
   test "should get index" do
-    get books_index_url
+    get books_url
     assert_response :success
+  end
+
+  test "should get show" do
+    get book_url(@book)
+    assert_response :success
+  end
+
+  test "should borrow a book" do
+    borrowing_params = attributes_for(:borrowing)
+    assert_difference("Borrowing.count") do
+      post borrow_book_url(@book), params: { borrowing: borrowing_params }
+    end
+
+    assert_redirected_to book_path(@book)
+  end
+
+  test "should return a borrowed book" do
+    borrowed_book = create(:borrowing, user: @user, book: @book)
+
+    patch return_borrowed_book_book_url(borrowed_book.book)
+
+    borrowed_book.reload
+    @book.reload
+
+    assert_not_nil borrowed_book.returned_at
+    assert_equal @book.status, "available"
+
+    assert_redirected_to user_path(@user)
   end
 end
