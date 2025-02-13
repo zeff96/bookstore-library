a984a8c881afaeb0945b7f26e636bb7acd4dc683
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:54:20 +0300

    Add controller test for show action which allows users to navigate to profile page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    sign_in @user
+  end
+  test "should navigate to user profile page" do
+    get user_path(@user)
+
+    assert_response :success
+  end
 end
