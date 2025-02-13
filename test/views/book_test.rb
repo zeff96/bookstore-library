34dc42ea1d96225a9f7f595c7a61959780f8e7f5
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:57:32 +0300

    Add view test for book index page and partial page

---
+++ test/views/book_test.rb
@@ -0,0 +1,18 @@
+require "views/view_test_helper"
+
+class BookViewTest < ViewTestCase
+  # test for book partial
+  test "renders a link to itself" do
+    book = create(:book)
+    render "books/book", book: book
+
+    assert_link book.title, href: book_path(book)
+  end
+
+  test "should render book author" do
+    book = create(:book)
+    render "books/book", book: book
+
+    assert_selector "span", text: book.author
+  end
+end
