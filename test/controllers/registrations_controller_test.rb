bfb95f9bf9015ec0a5d53013454cd5567d4a49ac
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:53:50 +0300

    Add controller test for create action of registrations controller

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+  end
+
+  test "should register user" do
+    user_params = attributes_for(:user)
+    assert_difference("User.count") do
+      post registration_url, params: { user: user_params }
+    end
+    assert_redirected_to root_url
+  end
 end
