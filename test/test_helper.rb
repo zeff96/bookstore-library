b036faea6ebf9ae5ee701a9712500c0202f3d8ab
zeff96 <zeffo1975@gmail.com>
2025-02-13 15:52:50 +0300

    Add support for factory bot syntax methods and sign in hleper method for test authentication

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "factory_bot_rails"
+require "lib/sign_in_helper_method"
 
 module ActiveSupport
   class TestCase
+    include FactoryBot::Syntax::Methods
+    include SignInHelperMethod
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
